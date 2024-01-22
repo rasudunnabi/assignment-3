b88f2e9cda91b7d358e5e921dad816ffb0325c4e
rasudunnabi <55378571+rasudunnabi@users.noreply.github.com>
2024-01-22 23:47:23 +0600

    added nav, slider, progress, program, products, FAQ sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  backgroundImage: {
+    "hero-pattern": "url('/img/hero-pattern.svg')",
+    "footer-texture": "url('/img/footer-texture.png')",
+  },
+};
