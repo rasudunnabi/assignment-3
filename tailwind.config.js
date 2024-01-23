c546ff8f842a980a5570f2fbd4b5954b13591987
rasudunnabi <55378571+rasudunnabi@users.noreply.github.com>
2024-01-23 16:39:39 +0600

    fixed some sizings and added hover effect on products

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-  backgroundImage: {
-    "hero-pattern": "url('/img/hero-pattern.svg')",
-    "footer-texture": "url('/img/footer-texture.png')",
-  },
 };
